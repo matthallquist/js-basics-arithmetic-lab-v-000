89e4e073233735c318d07997cfdb57b72a076846
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-25 17:00:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ const newID = oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
 let random = Math.random();
 const randomNumber = random * (20 - 1 + 1) + 1;
+const randomInteger = Math.floor(randomNumber);
